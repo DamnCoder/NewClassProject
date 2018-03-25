290b614567fb5e18dac8f74e8afc926fad43bfb2
DESKTOP-R2J65G1\jorge <jorge.lopez.glz@gmail.com>
2018-03-25 13:35:02 +0300

    + using %~dp0 which will give the full path to the batch file's directory

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,13 +1,21 @@
 @echo off
 
-cd..
-set directory=.\Build
+set bat_dir=%~dp0
+set build_dir=%bat_dir%..\Build
+set bin_path=%bat_dir%..\Build\NewClass.exe
 
-echo We need the folder: %directory%
+set cscomp=C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe
+set srcs=%bat_dir%..\NewClass\*.cs
 
-if not exist %directory% (
-	echo %directory% does not exists so we create it
-	mkdir %directory%
+echo We need the folder: %build_dir%
+
+if not exist %build_dir% (
+	echo %build_dir% does not exists so we create it
+	mkdir %build_dir%
 )
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /out:.\Build\NewClass.exe .\NewClass\*.cs
+echo We compile it
+%cscomp% /out:%bin_path% %srcs%
+echo Build finished
+
+::exit
